90b833228b5ae55b2856cf017910221417986d7a
Guilherme <guilhermev166@outlook.com>
2025-02-19 17:22:24 -0300

    new file:   exAula006/Downloads/Novo-Zelda.jpg 	modified:   exAula006/estilo.css 	modified:   exAula006/fim.php 	new file:   exAula006/form.php 	modified:   exAula006/index.php

--- exAula006/fim.php
+++ exAula006/fim.php
@@ -25,10 +25,47 @@
                     echo "<p><strong>Nome:</strong></p>$name <br>";
                     echo "<p><strong>Email:</strong></p> $email <br>";
                     echo "<p><strong>Telefone:</strong></p> $phone <br>";
-                    echo "<p><strong>Endereço:</strong></p> $street <br>";
+                    echo "<p><strong>Rua:</strong></p> $street <br>";
+                    echo "<p><strong>Cidade:</strong></p> $city <br>";
+                    echo "<p><strong>Estado:</strong></p> $stat <br>";
                     echo "<p><strong>Complemento:</strong></p> $complement <br>";
                     echo "<p><strong>Cep:</strong></p> $cep <br>";
-                }
+
+                    $folder = __DIR__ . "/Downloads/";
+
+                    if(!file_exists($folder) || !is_dir($folder)){
+                        mkdir($folder,  0755);
+                    }
+
+                    var_dump([
+                        "filesize" => ini_get("upload_max_filesize"),
+                        "postsize" => ini_get("post_max_size"),
+                    ]);
+
+                    $getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
+                    var_dump($_FILES);
+
+                    if($_FILES && !empty($_FILES['file']['name'])) {
+                        $fileUpload = $_FILES['file'];
+                        var_dump($_FILES);
+                    
+                        $allowebTypes = [ 
+                            "image/jpeg",
+                            "image/png",
+                            "application/pdf"
+                        ];
+                    }elseif ( $getPost){ 
+                        echo "<p class='trigger warning'>Whoops parece que o arquivo que você selecionou é grande demais!</p>";
+                    } else{
+                       if($_FILES){
+                           echo "<p class='trigger warning'>Selecione um arquivo antes de Enviar!</p>";
+                       }
+                    }
+
+                    include __DIR__ . "/form.php";
+
+                    var_dump(scandir(__DIR__ ."/Downloads"));
+                }    
                 ?>
             </article>
         </main>
--- exAula006/index.php
+++ exAula006/index.php
@@ -30,6 +30,11 @@
                         <label for="cep">Cep</label>
                         <input type="text" name="cep" id="idcep" required><br><br>
                         <input type="submit" value="Enviar">
+                        <form name="post" action="./?post=true" method="post" enctype="multipart/form-data">
+                        <p style="margin-bottom: 10px; text-align: right"><a href="./" title="Atualizar">Atualizar</a></p>
+                        <input type="file" name="file"/>
+                        <button>Enviar</button>
+                        </form>
                     </div>
             </form>
         </section>
