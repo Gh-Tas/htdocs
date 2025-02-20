e144eaea0694310311ba9833e6b78a36e235d409
Guilherme <guilhermev116@outlook.com>
2025-02-20 15:40:26 -0300

    atualização

--- exAula006/fim.php
+++ exAula006/fim.php
@@ -45,21 +45,39 @@
                     $getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
                     var_dump($_FILES);
 
-                    if($_FILES && !empty($_FILES['file']['name'])) {
-                        $fileUpload = $_FILES['file'];
-                        var_dump($_FILES);
+                    $estado = '';
+
+                    if ($_FILES && !empty($_FILES['file']['name'])) {
+                        $estado = 'arquivo_enviado';
+                    } elseif ($getPost) {
+                        $estado = 'arquivo_grande';
+                    } elseif ($_FILES) {
+                        $estado = 'arquivo_nao_selecionado';
+                    }
                     
-                        $allowebTypes = [ 
-                            "image/jpeg",
-                            "image/png",
-                            "application/pdf"
-                        ];
-                    }elseif ( $getPost){ 
-                        echo "<p class='trigger warning'>Whoops parece que o arquivo que você selecionou é grande demais!</p>";
-                    } else{
-                       if($_FILES){
-                           echo "<p class='trigger warning'>Selecione um arquivo antes de Enviar!</p>";
-                       }
+                    $acoes = [
+                        'arquivo_enviado' => function () {
+                            $fileUpload = $_FILES['file'];
+                            var_dump($_FILES);
+                    
+                            $allowebTypes = [
+                                "image/jpeg",
+                                "image/png",
+                                "application/pdf"
+                            ];
+                        },
+                    
+                        'arquivo_grande' => function () {
+                            echo "<p class='trigger warning'>Whoops parece que o arquivo que você selecionou é grande demais!</p>";
+                        },
+                    
+                        'arquivo_nao_selecionado' => function () {
+                            echo "<p class='trigger warning'>Selecione um arquivo antes de Enviar!</p>";
+                        },
+                    ];
+                    
+                    if (isset($acoes[$estado])) {
+                        $acoes[$estado](); // Executa a função correspondente ao estado
                     }
 
                     include __DIR__ . "/form.php";
